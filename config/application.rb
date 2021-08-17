4aaef53a90ac4518a9b32b298fbb8446f8c3a3f7
umarkotak <umarkotak@gmail.com>
2021-08-17 12:51:55 +0700

    conditional dotenv load

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 module Dexpense
   class Application < Rails::Application
