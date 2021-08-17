9372eac83628f5b0f77617fe500cc9d390696d11
umar.ramadhana <umar.ramadhana@gmail.com>
2021-08-17 10:35:10 +0700

    fix transaction validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,16 +4,16 @@ class Transaction < ApplicationRecord
     "daily_needs",
     "other"
   ].freeze
-  
+
   validates :category, :amount, :direction_type, :name,
             presence: true
   validates :category,
             inclusion: { in: TRANSACTION_CATEGORIES }
   validates :amount,
             numericality: {greater_than_or_equal_to: 0}
-  validates :direction,
-            inclusion: { in %w(income outcome) }
-  
+  validates :direction_type,
+            inclusion: { in: %w(income outcome) }
+
   belongs_to :account
   belongs_to :group
   belongs_to :group_wallet
