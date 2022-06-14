740aa7046dcaa2551290d675a837b6e8bc419c49
umar ramadhana <umar.ramadhana@gojek.com>
2022-06-14 09:31:33 +0700

    fix const

--- app/models/const.rb
+++ app/models/const.rb
@@ -41,59 +41,59 @@ class Const < ActiveRecord::Base
       "sub_categories_map": {
         "antam_0.5g": {
           "weight": 0.5,
-          "unit": "gram"
-          "sub_category": "antam_0.5g"
+          "unit": "gram",
+          "sub_category": "antam_0.5g",
           "id": "Antam 0.5g",
-          "id": "Antam 0.5g",
+          "en": "Antam 0.5g",
         },
         "antam_1g": {
           "weight": 1,
-          "unit": "gram"
-          "sub_category": "antam_1g"
+          "unit": "gram",
+          "sub_category": "antam_1g",
           "id": "Antam 1g",
-          "id": "Antam 1g",
+          "en": "Antam 1g",
         },
         "antam_2g": {
           "weight": 2,
-          "unit": "gram"
-          "sub_category": "antam_2g"
+          "unit": "gram",
+          "sub_category": "antam_2g",
           "id": "Antam 2g",
-          "id": "Antam 2g",
+          "en": "Antam 2g",
         },
         "antam_3g": {
           "weight": 3,
-          "unit": "gram"
-          "sub_category": "antam_3g"
+          "unit": "gram",
+          "sub_category": "antam_3g",
           "id": "Antam 3g",
-          "id": "Antam 3g",
+          "en": "Antam 3g",
         },
         "antam_10g": {
           "weight": 10,
-          "unit": "gram"
-          "sub_category": "antam_10g"
+          "unit": "gram",
+          "sub_category": "antam_10g",
           "id": "Antam 10g",
-          "id": "Antam 10g",
+          "en": "Antam 10g",
         },
         "antam_25g": {
           "weight": 25,
-          "unit": "gram"
-          "sub_category": "antam_25g"
+          "unit": "gram",
+          "sub_category": "antam_25g",
           "id": "Antam 25g",
-          "id": "Antam 25g",
+          "en": "Antam 25g",
         },
         "antam_50g": {
           "weight": 50,
-          "unit": "gram"
-          "sub_category": "antam_50g"
+          "unit": "gram",
+          "sub_category": "antam_50g",
           "id": "Antam 50g",
-          "id": "Antam 50g",
+          "en": "Antam 50g",
         },
         "antam_100g": {
           "weight": 100,
-          "unit": "gram"
-          "sub_category": "antam_100g"
+          "unit": "gram",
+          "sub_category": "antam_100g",
           "id": "Antam 100g",
-          "id": "Antam 100g",
+          "en": "Antam 100g",
         },
       }
     }
